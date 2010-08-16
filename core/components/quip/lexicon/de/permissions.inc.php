bb3e5d639277d88b71a176780dfca2da93c5a7af
denschub <software@dsx.cc>
2010-08-17 03:18:39 +0800

    - Fully translated permissions.inc.php

--- core/components/quip/lexicon/de/permissions.inc.php
+++ core/components/quip/lexicon/de/permissions.inc.php
@@ -22,16 +22,18 @@
  * @package quip
  */
 /**
+ * Translated by Dennis "DenSchub" Schubert <software@dsx.cc>
+ *
  * @package quip
  * @subpackage lexicon
  */
-$_lang['perm.comment_approve'] = 'Approve a comment.';
-$_lang['perm.comment_list'] = 'List comments for a thread.';
-$_lang['perm.comment_list_unapproved'] = 'List unapproved comments.';
-$_lang['perm.comment_remove'] = 'Remove a comment.';
-$_lang['perm.comment_update'] = 'Update a comment.';
-$_lang['perm.thread_list'] = 'List available threads.';
-$_lang['perm.thread_manage'] = 'Manage a thread.';
-$_lang['perm.thread_truncate'] = 'Truncate all a thread\'s comments.';
-$_lang['perm.thread_update'] = 'Update a thread.';
-$_lang['perm.thread_view'] = 'View a thread.';
+$_lang['perm.comment_approve'] = 'Kommentar genehmigen.';
+$_lang['perm.comment_list'] = 'Kommentare im Thread anzeigen.';
+$_lang['perm.comment_list_unapproved'] = 'Ungenehmigte Kommentare anzeigen.';
+$_lang['perm.comment_remove'] = 'Kommentare entfernen.';
+$_lang['perm.comment_update'] = 'Kommentare bearbeiten.';
+$_lang['perm.thread_list'] = 'Verfügbare Threads anzeigen.';
+$_lang['perm.thread_manage'] = 'Threads verwalten.';
+$_lang['perm.thread_truncate'] = 'Thread leeren';
+$_lang['perm.thread_update'] = 'Thread bearbeiten.';
+$_lang['perm.thread_view'] = 'Thread anzeigen.';
